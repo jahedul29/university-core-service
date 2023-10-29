5d159a52c1da2c6ed0a5b930ce050ae1272954bf
Jahedul Hoque <jahedulh1@gmail.com>
2023-10-29 07:20:49 +0100

    some update done

--- src/app/modules/academicSemester/academicSemester.constants.ts
+++ src/app/modules/academicSemester/academicSemester.constants.ts
@@ -11,3 +11,31 @@ export const academicSemesterFilterableFields = [
   'startMonth',
   'endMonth',
 ];
+
+export const academicSemesterTitleCodeMapper: { [key: string]: string } = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
+
+export const monthList: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const academicSemesterTitles: string[] = ['Autumn', 'Summer', 'Fall'];
+export const academicSemesterCodes: string[] = ['01', '02', '03'];
+
+export const EVENT_ACADEMIC_SEMESTER_CREATED = 'academic-semester.created';
+export const EVENT_ACADEMIC_SEMESTER_UPDATED = 'academic-semester.updated';
+export const EVENT_ACADEMIC_SEMESTER_DELETED = 'academic-semester.deleted';
